e2e7927b003f7167e3ec111a67eabdd50e9ee50d
Marten de Bruijn <martendebruijn@outlook.com>
2023-11-14 15:29:20 +0100

    docs: add docstrings

--- number_guessing/number_guessing.py
+++ number_guessing/number_guessing.py
@@ -1,8 +1,31 @@
+"""Terminal number guessing game.
+
+This script allows the user to play a game where the player needs to guess a secret
+number.
+"""
+
 import sys
 import random
 
 
-def ask_for_int(question):
+def ask_for_int(question: str) -> int | None:
+    """Ask for input that must be an integer.
+
+    Parameters
+    ----------
+    question : str
+        The question being asked.
+
+    Returns
+    -------
+    int
+        The inputted integer.
+
+    Raises
+    ------
+    ValueError
+        If inputted value is not an integer.
+    """
     answer = input(f"{question}\n")
     try:
         return int(answer)
@@ -11,12 +34,26 @@ def ask_for_int(question):
         ask_for_int("Please try again.")
 
 
-def change_settings(min, max):
+def change_settings(min: int, max: int) -> tuple[int, int] | None:
+    """Ask to change the range for the game.
+
+    Parameters
+    ----------
+    min : int
+        The minimum value.
+    max : int
+        The maximum value.
+
+    Returns
+    -------
+    tuple of int
+        (min, max)
+    """
     answer = input(
         f"\nThe default is a target number between {min} and {max}, inclusive.\nDo you like to change these? (n/y)\n"
     )
     if answer.lower() == "n":
-        minmax = [min, max]
+        minmax = (min, max)
     elif answer.lower() == "y":
         minmax = change_min_max()
     else:
@@ -25,7 +62,14 @@ def change_settings(min, max):
     return minmax
 
 
-def change_min_max():
+def change_min_max() -> tuple[int | None, int | None] | None:
+    """Change the min and max values for the game.
+
+    Returns
+    -------
+    tuple of int
+        (min, max)
+    """
     min = ask_for_int("\nPlease enter a minimum value:")
     max = ask_for_int("\nPlease enter a maximum value:")
     if max <= min:
@@ -34,14 +78,38 @@ def change_min_max():
         )
         change_min_max()
     else:
-        return [min, max]
+        return (min, max)
 
 
-def start_game(options={"min": 1, "max": 100}):
+def start_game(options: dict[int, int] = {"min": 1, "max": 100}):
+    """Start a game
+
+    Parameters
+    ----------
+    options : dict, optional, default: { "min": 1, "max": 100 }
+        The default range of the game.
+
+    Returns
+    -------
+    function
+        The function that controls making a guess.
+    """
     min, max = change_settings(min=options["min"], max=options["max"])
     target = random.randint(min, max)
 
-    def make_guess(round=1):
+    def make_guess(round: int = 1) -> None:
+        """Make a guess.
+
+        Parameters
+        ----------
+        round : int, optional, default: 1
+            The current round number
+
+        Raises
+        ------
+        ValueError
+            If inputted value is not an integer.
+        """
         print(f"\nRound: {round}")
         guess = input("Please enter your guess:\n")
 
@@ -62,7 +130,8 @@ def start_game(options={"min": 1, "max": 100}):
     return make_guess
 
 
-def ask_play_again():
+def ask_play_again() -> None:
+    """Ask player to play again."""
     new_game = input("\nDo you want to play again? (n/y)\n")
     if new_game.lower() == "y":
         start_game()()
